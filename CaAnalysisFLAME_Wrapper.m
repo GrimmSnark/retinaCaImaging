7492d306b72ed371bdfaad86684827bb2d5e6a01
Michael Savage <michael.savage2@ncl.ac.uk>
2023-09-08 14:55:57 +0100

    Bug fix again

--- CaAnalysisFLAME_Wrapper.m
+++ CaAnalysisFLAME_Wrapper.m
@@ -1,11 +1,15 @@
-function CaAnalysisFLAME_Wrapper(exStructPath, baselineSubType)
+function CaAnalysisFLAME_Wrapper(exStructPath, fileStartNo, baselineSubType)
 
 if nargin < 1 || isempty(exStructPath)
     exStructPath= uigetdir([],...
         'Image File Selector');
 end
 
-if nargin < 2 || isempty(baselineSubType)
+if nargin < 2 || isempty(fileStartNo)
+    fileStartNo = 1;
+end
+
+if nargin < 3 || isempty(baselineSubType)
     baselineSubType = 2;
 end
 
